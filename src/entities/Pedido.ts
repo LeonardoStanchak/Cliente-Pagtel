aa0b8a452d2390767e495d00ffa9feb176145af5
Leonardo Stanchak <leonardostanchak@hotmail.com>
2022-08-09 09:56:41 -0300

    Criação da classe produto

--- src/entities/Pedido.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-@Entity()
-export class Pedido{
-    @PrimaryGeneratedColumn()
-    id: number;
-    @Column({type: "integer"})
-    numero_pedido: number;
-}
